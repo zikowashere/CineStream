692c52b50d8b2c492a6dbd17e694983a07f3f457
zikowashere <zakdaouma@gmail.com>
2023-10-12 10:05:43 +0200

    fix : bug authorize next auth

--- cinestream/app/optionAuth/optionAuth.ts
+++ cinestream/app/optionAuth/optionAuth.ts
@@ -1,9 +1,19 @@
 import axios, { AxiosError } from "axios";
+import { Session } from "inspector";
 import jwt from "jsonwebtoken";
 import { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { decode } from "../type/decode";
+import { user } from "../type/user";
+
+type User = {
+  id: string;
+  name: string;
+  email: string | null;
+  image: string | null;
+  token: string;
+};
 
 export const optionAuth: NextAuthOptions = {
   providers: [
@@ -12,6 +22,19 @@ export const optionAuth: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     Credentials({
+      credentials: {
+        firstName: {
+          label: "firstName",
+          type: "text",
+          placeholder: "first name",
+        },
+        lastName: { label: "lastName", type: "text", placeholder: "last name" },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
       async authorize(credentials, req) {
         try {
           const userResponse = await axios.post(
@@ -24,25 +47,30 @@ export const optionAuth: NextAuthOptions = {
               },
             }
           );
+          if (!userResponse) return null;
           const token = userResponse.data.token;
           const decoded = jwt.decode(token) as decode;
           const user = decoded?.user;
 
           if (user) {
-            const shapedUser = {
+            const shapedUser: User = {
+              id: user.id,
               name: `${user.firstName} ${user.lastName}`,
               email: null,
               image: null,
+              token: token,
             };
 
-            return Promise.resolve({ ...shapedUser, token });
+            return Promise.resolve<User>(shapedUser);
           }
+          return null;
         } catch (error) {
           if (error instanceof AxiosError) {
             const axiosError = error as AxiosError;
             return Promise.reject(axiosError.response?.data);
           }
         }
+        return null;
       },
     }),
   ],
